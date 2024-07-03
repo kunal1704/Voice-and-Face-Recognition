fba3b7f662b39070e288863cedcfbbe6c29d92c6
Aaryanajith <aaryanajith45@gmail.com>
2024-07-03 17:34:11 +0530

    fix: added code to remove sample file if it exists

--- dsp.m
+++ dsp.m
@@ -6,7 +6,8 @@ num_users = 3;
 num_samples = 5; 
 
 
-data_dir = '/home/interstellar/DSP_proj/'; % Add your file path where the audio file is getting stored.
+
+data_dir = '/home/interstellar/DSP_proj/'; % Add your file path where the program is running.
 
 
 function record_sample(file_path)
@@ -28,11 +29,10 @@ for i = 1:num_users
     end
 end
 
-
 features = [];
 labels = [];
 
-
+% Extract features from the recorded samples
 for i = 1:num_users
     for j = 1:num_samples
         file_path = [data_dir 'user' num2str(i) '_sample' num2str(j) '.wav'];
@@ -48,23 +48,27 @@ for i = 1:num_users
     end
 end
 
-
 if size(features, 1) <= size(features, 2)
     error('Not enough valid feature vectors to fit the model.');
 end
 
-
+% Trains the SVM model
 SVMModel = fitcecoc(features, labels);
 
+test_sample_path = [data_dir 'test_sample.wav'];
+if exist(test_sample_path, 'file')
+    delete(test_sample_path);
+end
 
 disp('Recording test sample:');
-record_sample([data_dir 'test_sample.wav']);
+record_sample(test_sample_path);
 
-
-[y, Fs] = audioread([data_dir 'test_sample.wav']);
+% This extracts features from the test sample
+[y, Fs] = audioread(test_sample_path);
 coeffs = mfcc(y, Fs);
 meanCoeffs = mean(coeffs, 1);
 
+% This predicts which user it is
 label = predict(SVMModel, meanCoeffs);
 
 disp(['Identified User: ' num2str(label)]);
